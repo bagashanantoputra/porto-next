2292a235b26fae44369c01ece4927b6e9eefb830
bagashanantoputra <danandjaya123@gmail.com>
2024-09-09 05:27:52 +0700

    fix navbar

--- components/MobileNav.jsx
+++ components/MobileNav.jsx
@@ -1,9 +1,10 @@
 "use client"
 
-import { Sheet, SheetContent, SheetTrigger } from "./ui/sheet"
-import { usePathname } from "next/navigation"
-import Link from "next/link"
-import { CiMenuFries } from "react-icons/ci"
+import { useState } from "react";
+import { Sheet, SheetContent, SheetTrigger } from "./ui/sheet";
+import { usePathname } from "next/navigation";
+import Link from "next/link";
+import { CiMenuFries } from "react-icons/ci";
 
 const links = [
     {
@@ -26,16 +27,23 @@ const links = [
         name: "contact",
         path: "/contact"
     },
-]
+];
+
 const MobileNav = () => {
     const pathname = usePathname();
+    const [isOpen, setIsOpen] = useState(false);
+
+    const handleLinkClick = () => {
+        setIsOpen(false); // Close the menu when a link is clicked
+    };
+
     return (
-        <Sheet>
+        <Sheet open={isOpen} onOpenChange={setIsOpen}>
             <SheetTrigger className="flex justify-center items-center">
                 <CiMenuFries className="text-[32px] text-accent"/>
             </SheetTrigger>
             <SheetContent aria-label="Mobile navigation menu" className="flex flex-col">
-                <div className="mt-32 mb-40 text-center text-2xl">
+                <div className="mt-32 mb-32 text-center text-2xl">
                     <Link href="/">
                         <h1 className="text-4xl font-semibold">
                             Bagas<span className="text-accent">.</span>
@@ -44,17 +52,20 @@ const MobileNav = () => {
                 </div>
 
                 <nav className="flex flex-col justify-center items-center gap-8">
-                    {links.map((link, index)=>{
-                        return(
-                            <Link href={link.path} key={index} className={`${link.path === pathname && "text-accent border-b-2 border-accent"}text-xl capitalize hover:text-accent transition-all`}>
-                                {link.name}
-                            </Link>
-                        )
-                    })}
+                    {links.map((link, index) => (
+                        <Link 
+                            href={link.path} 
+                            key={index} 
+                            onClick={handleLinkClick}
+                            className={`${link.path === pathname && "text-accent border-b-2 border-accent"} text-xl capitalize hover:text-accent transition-all`}
+                        >
+                            {link.name}
+                        </Link>
+                    ))}
                 </nav>
             </SheetContent>
         </Sheet>
-    )
-}
+    );
+};
 
-export default MobileNav
+export default MobileNav;
